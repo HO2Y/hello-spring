14184e17308c5e271841ea646d61e3ce65f91d82
HO2Y <bhyhy1224@gmail.com>
2022-07-18 14:20:44 +0900

    회원 서비스 개발

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,43 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     *회원 가입
+     */
+
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) { //method 변환시 ctrl+shift+alt+t
+        memberRepository.findByName(member.getName()) //Optional 안에 Member 객체가 있는것
+                .ifPresent(m -> { //null 아니라 값이 있으면 아래 로직 동작 / Optional 이기에 가능
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
+
