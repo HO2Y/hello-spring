e4e2e2eb41228ac247a68840f024c872ebfed71c
HO2Y <bhyhy1224@gmail.com>
2022-07-18 11:55:50 +0900

    회원 리포지토리 테스트 리포트 작성 및 domain 수정

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,35 +1,34 @@
 package hello.hellospring.repository;
-
 import hello.hellospring.domain.Member;
-
 import java.util.*;
-
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ */
 public class MemoryMemberRepository implements MemberRepository {
-
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
-
     @Override
-    public Member save(Member member) { //System 식별용 지정
+    public Member save(Member member) {
         member.setId(++sequence);
         store.put(member.getId(), member);
         return member;
     }
-
     @Override
-    public Optional<Member> findById(Long id) { //optional = null 반환할때 optional 붙여서 반환
+    public Optional<Member> findById(Long id) {
         return Optional.ofNullable(store.get(id));
     }
-
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
     @Override
     public Optional<Member> findByName(String name) {
-        return store.values().stream() //루프로 돌리는 거
+        return store.values().stream()
                 .filter(member -> member.getName().equals(name))
                 .findAny();
     }
 
-    @Override
-    public List<Member> findAll() {
-        return new ArrayList<>(store.values());
+    public void clearStore() {
+        store.clear();
     }
 }
