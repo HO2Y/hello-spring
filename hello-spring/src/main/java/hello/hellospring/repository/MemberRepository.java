fa532785ecd47e7276f423bf3d46bd3c5c8bb124
HO2Y <bhyhy1224@gmail.com>
2022-08-17 11:25:50 +0900

    h2 DB연결 순수 JDBC 이용

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 public interface MemberRepository {
     Member save(Member member);
-    Optional<Member> findById(Long id);
+    Optional<Member> findById(Long id); //optional = null 반환할때 optional 붙여서 반환
     Optional<Member> findByName(String name);
     List<Member> findAll();
 }
